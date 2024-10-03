9fe806415da00e46c50db466c6d5962c7277677d
mayuri_mundada <mayurimundada13@gmail.com>
2024-10-03 14:27:27 +0530

    committing cicd project using code deploy

---
+++ src/main/java/com/docker/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/get-user")
+    public String getUser() {
+        return "Hello, Mayuri Mundada..!!";
+    }
+}
